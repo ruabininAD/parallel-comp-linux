fdb179bf78d8ef3a13bcec65c96882b6883cb82e
ruabininAD <ryabinin.ad@edu.spbstu.ru>
2023-06-29 02:27:24 +0300

    f

--- src/openmp.cpp
+++ src/openmp.cpp
@@ -1,64 +1,22 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/asio.hpp>
-#include <omp.h>
 #include <iostream>
+#include <omp.h>
 
-namespace http = boost::beast::http;
-namespace net = boost::asio;
-using tcp = boost::asio::ip::tcp;
+int main() {
+    // Установка количества потоков
+    omp_set_num_threads(4);
 
-void handle_request(http::request<http::dynamic_body>& request, http::response<http::dynamic_body>& response)
-{
-    // Здесь вы можете выполнять вычисления для обработки запроса
-    // Например, вы можете вызывать функции, реализующие вашу логику
-
-    // Пример: Возврат простого HTML-ответа
-    response.body() = "<html><body><h1>Hello, World!</h1></body></html>";
-    response.prepare_payload();
-}
-
-void handle_session(tcp::socket& socket)
-{
-    boost::beast::flat_buffer buffer;
-    http::request<http::dynamic_body> request;
-    http::response<http::dynamic_body> response;
-
-    try
+    #pragma omp parallel
     {
-        http::read(socket, buffer, request);
-        handle_request(request, response);
-        http::write(socket, response);
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-}
+        // Получение идентификатора потока
+        int thread_id = omp_get_thread_num();
 
-int main()
-{
-    // Создание объекта io_context для обработки сетевых операций
-    net::io_context ioc;
+        // Вывод сообщения с использованием идентификатора потока
+        std::cout << "Hello from thread " << thread_id << std::endl;
 
-    // Создание объекта acceptor для прослушивания входящих соединений
-    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 8080));
-
-    // Бесконечный цикл обработки входящих соединений
-    while (true)
-    {
-        // Ожидание нового входящего соединения
-        tcp::socket socket(ioc);
-        acceptor.accept(socket);
-
-        // Обработка соединения в отдельном потоке с использованием OpenMP
-        #pragma omp parallel num_threads(omp_get_max_threads())
-        {
-            #pragma omp single
-            {
-                handle_session(socket);
-            }
+        // Параллельный цикл
+        #pragma omp for
+        for (int i = 0; i < 10; ++i) {
+            std::cout << "Thread " << thread_id << ": Iteration " << i << std::endl;
         }
     }
 
